bf33bbada6703e87348e668afdfa0c4ecb7e025d
AbdulRafay-00 <iamrafay.00@gmail.com>
2026-05-22 23:48:23 +0500

    service repository added

---
+++ Service-Registry/src/test/java/com/rafay/Service_Registry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafay.Service_Registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
