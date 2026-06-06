35e42bbd48df7b79cecf44b3f8ade766682b8017
AbdulRafay-00 <iamrafay.00@gmail.com>
2026-06-07 03:35:47 +0500

    paring service was added

---
+++ Pairing-Service/src/main/java/com/rafay/Pairing_Service/PairingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.rafay.Pairing_Service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PairingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PairingServiceApplication.class, args);
+	}
+
+}
