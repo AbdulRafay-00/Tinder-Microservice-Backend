60b183e88388722ec8e332b186bfd6e147d48dab
AbdulRafay-00 <iamrafay.00@gmail.com>
2026-07-24 04:14:06 +0500

    USER-SERVICE: Unit test case 2 of  LoginService is added

--- user-service/src/test/java/com/rafay/user_service/service/LoginSignupService/LoginServicesTest.java
+++ user-service/src/test/java/com/rafay/user_service/service/LoginSignupService/LoginServicesTest.java
@@ -1,6 +1,7 @@
 package com.rafay.user_service.service.LoginSignupService;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.*;
 
 import java.util.Optional;
@@ -33,35 +34,47 @@ public class LoginServicesTest {
 
     @Test
     void testVerifyCredential() {
-        AuthCredentials authCredentials = new AuthCredentials( "rafay@example.com", "12345");
+        AuthCredentials authCredentials = new AuthCredentials("rafay@example.com", "12345");
         LoginDto loginDto = new LoginDto();
         loginDto.setEmail("rafay@example.com");
         loginDto.setPassword("12345");
+// why did't we use this bec authenticationmanager is a mock obj not a real object 
+// it will not check the value it will just check the refrence and below we are creating
+// new obj so comparing refrence wo fail thats why we are not using this
+        // when(authenticationManager.authenticate(
+        //         new UsernamePasswordAuthenticationToken(
+        //                 loginDto.getEmail(),
+        //                 loginDto.getPassword())))
+        //         .thenReturn(null);
 
-        when(authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(
-                    loginDto.getEmail(),
-                    loginDto.getPassword()))).thenReturn(null);
+
+        when (authenticationManager.authenticate(any(UsernamePasswordAuthenticationToken.class)))
+                .thenReturn(null);
 
         when(authCredentialsRepository.findByUserEmail(
-            loginDto.getEmail())).thenReturn(Optional.of(authCredentials));
-
+                loginDto.getEmail())).thenReturn(Optional.of(authCredentials));
 
         when(jwtServices.jwt_token_gen(authCredentials, authCredentials.getUserProfileDB()))
                 .thenReturn("mocked-jwt-token");
-
-
 
         String result = loginServices.verifyCredential(loginDto);
 
         assertEquals("mocked-jwt-token", result);
 
-
     }
 
+    @Test
+    void testVerifyCredential_invalidCredentials() {
+        LoginDto loginDto = new LoginDto();
+        loginDto.setEmail("rafay@example.com");
+        loginDto.setPassword("wrongpass");
 
+        when(authenticationManager.authenticate(any(UsernamePasswordAuthenticationToken.class)))
+                .thenThrow(new RuntimeException("Bad credentials"));
 
+        String result = loginServices.verifyCredential(loginDto);
 
+        assertEquals("Invalid email or password", result);
+    }
 
-    
 }
