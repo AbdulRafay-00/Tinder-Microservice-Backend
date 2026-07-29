ad31a666fc3daf8f24aaea2fae7abaeadc902dca
AbdulRafay-00 <iamrafay.00@gmail.com>
2026-07-30 01:17:30 +0500

    Unit Testing for Folder Service on the  File PairingService

---
+++ Pairing-Service/src/test/java/com/rafay/PairingService/service/PairingServiceTest.java
@@ -0,0 +1,46 @@
+package com.rafay.PairingService.service;
+
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.rafay.PairingService.DB.PairDB;
+import com.rafay.PairingService.kafka.notifyemailproducer.PairingEventProducer;
+import com.rafay.PairingService.repository.PairDBRepository;
+
+@ExtendWith(MockitoExtension.class)
+public class PairingServiceTest {
+
+    @Mock
+    private PairDBRepository pairDBRepository;
+    @Mock
+    private PairingEventProducer pairingEventProducer;
+
+    @InjectMocks
+    private PairingService pairingService;
+    
+    @Test
+    void testSavePairingEvent() {
+        // Assert
+        String swiperId = "swiper123";
+        String swipedId = "swiped456";
+        // when(pairDBRepository.save(any(PairDB.class))).thenAnswer(invocation -> invocation.getArgument(0));
+        when(pairDBRepository.save(any(PairDB.class))).thenReturn(new PairDB());
+        // Act
+        pairingService.savePairingEvent(swiperId, swipedId);
+
+        // 
+        verify(pairDBRepository, times(1)).save(any(PairDB.class));
+        verify(pairingEventProducer, times(1)).sendPairingEvent(swiperId, swipedId);
+
+        
+
+
+        
+    }
+}
