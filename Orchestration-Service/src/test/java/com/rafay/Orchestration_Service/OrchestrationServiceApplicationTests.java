a0b3b4b6f036974570358a4f911085ce9978a853
AbdulRafay-00 <iamrafay.00@gmail.com>
2026-08-09 19:29:37 +0500

    Integrtion Testing for Orchestration-service setup of dumy container done

--- Orchestration-Service/src/test/java/com/rafay/Orchestration_Service/OrchestrationServiceApplicationTests.java
+++ Orchestration-Service/src/test/java/com/rafay/Orchestration_Service/OrchestrationServiceApplicationTests.java
@@ -3,8 +3,10 @@ package com.rafay.Orchestration_Service;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.rafay.Orchestration_Service.resources.testcontainerconfig.BaseIntegrationTest;
+
 @SpringBootTest
-class OrchestrationServiceApplicationTests {
+class OrchestrationServiceApplicationTests extends BaseIntegrationTest {
 
 	@Test
 	void contextLoads() {
---
+++ Orchestration-Service/src/test/java/com/rafay/Orchestration_Service/controller/UserReqIT.java
@@ -0,0 +1,13 @@
+package com.rafay.Orchestration_Service.controller;
+
+import org.junit.Test;
+
+import com.rafay.Orchestration_Service.resources.testcontainerconfig.BaseIntegrationTest;
+
+public class UserReqIT extends BaseIntegrationTest {
+
+    @Test
+    public void redisCacheHitS(){
+        
+    }   
+}
