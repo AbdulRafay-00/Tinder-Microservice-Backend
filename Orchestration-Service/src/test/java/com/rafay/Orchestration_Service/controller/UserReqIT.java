b0f2c800b9a9a2c40b2e8a6c8a64d5144fe9a510
AbdulRafay-00 <iamrafay.00@gmail.com>
2026-08-09 21:35:55 +0500

    Integrtion Testing for Orchestration-service test case two out of two done

--- Orchestration-Service/src/test/java/com/rafay/Orchestration_Service/controller/UserReqIT.java
+++ Orchestration-Service/src/test/java/com/rafay/Orchestration_Service/controller/UserReqIT.java
@@ -1,5 +1,7 @@
 package com.rafay.Orchestration_Service.controller;
 
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -9,13 +11,26 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import com.rafay.Orchestration_Service.DTO.NearLocationRequestDTO;
+import com.rafay.Orchestration_Service.DTO.NearbySearchResultDto;
+import com.rafay.Orchestration_Service.FeignClients.NearByUserEvent.FilteredList;
+import com.rafay.Orchestration_Service.FeignClients.NearByUserEvent.NearByReq;
 import com.rafay.Orchestration_Service.testcontainerconfig.BaseIntegrationTest;
 
 public class UserReqIT extends BaseIntegrationTest {
     @Autowired
     MockMvc mockMvc;
+
+    @MockitoBean
+    NearByReq nearByReq;
+
+    @MockitoBean
+    FilteredList filteredList;
+
     @Autowired
     RedisTemplate<String, String> redisTemplate;
 
@@ -41,7 +56,47 @@ public class UserReqIT extends BaseIntegrationTest {
         .contentType(MediaType.APPLICATION_JSON)
         .content(requestBody)
         .header("X-User-Id", userId)).andExpect(status().isOk());
+    }
 
 
+    @Test
+    void redisCacheMiss() throws Exception {
+
+        String userId = "user123";
+
+        // Location Service response
+        NearbySearchResultDto locationResult =
+                new NearbySearchResultDto(
+                        userId,
+                        List.of("user1", "user2", "user3")
+                );
+
+        when(nearByReq.getNearbySearchSync(any(NearLocationRequestDTO.class)))
+                .thenReturn(ResponseEntity.ok(locationResult));
+
+        // Match Service response after filtering
+        NearbySearchResultDto filteredResult =
+                new NearbySearchResultDto(
+                        userId,
+                        List.of("user1", "user3")
+                );
+
+        when(filteredList.acceptNearbyUsers(any(NearbySearchResultDto.class)))
+                .thenReturn(ResponseEntity.ok(filteredResult));
+
+        String requestBody = """
+                {
+                    "latitude": 40.7128,
+                    "longitude": -74.0060
+                }
+                """;
+
+        mockMvc.perform(
+                post("/discovery")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestBody)
+                        .header("X-User-Id", userId)
+        )
+        .andExpect(status().isOk());
     }
 }
