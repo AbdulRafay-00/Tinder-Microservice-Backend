868ea4351f8070485b477c1215d8d56d439f2024
AbdulRafay-00 <iamrafay.00@gmail.com>
2026-08-13 04:16:08 +0500

    PARING-SERVICE Integration Testing ParingServiceKAfkaListner tested status:Success

---
+++ Pairing-Service/src/test/java/com/rafay/PairingService/kafka/notifyemailproducer/PairingEventProducerTest.java
@@ -0,0 +1,12 @@
+package com.rafay.PairingService.kafka.notifyemailproducer;
+
+import org.junit.jupiter.api.Test;
+
+import com.rafay.PairingService.testconfig.ContainerInfo;
+
+public class PairingEventProducerTest extends ContainerInfo {
+    @Test
+    void testSendPairingEvent() {
+
+    }
+}
--- Pairing-Service/src/test/java/com/rafay/PairingService/testconfig/ContainerInfo.java
+++ Pairing-Service/src/test/java/com/rafay/PairingService/testconfig/ContainerInfo.java
@@ -5,6 +5,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.kafka.KafkaContainer;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.junit.jupiter.Container;
@@ -26,4 +28,22 @@ public abstract class ContainerInfo {
     protected
     static KafkaContainer kafka = new KafkaContainer(
             DockerImageName.parse("apache/kafka:4.2.0"));
+
+        @DynamicPropertySource
+    static void overrideProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.kafka.bootstrap-servers", kafka::getBootstrapServers);
+        // add mysql equivalents here too if your manual config also reads datasource props via @Value
+    }
 }
+//         @DynamicPropertySource
+//     static void overrideProperties(DynamicPropertyRegistry registry) {
+//         registry.add("spring.kafka.bootstrap-servers", kafka::getBootstrapServers);
+//         // wire MySQL container properties into Spring Boot test context
+//         registry.add("spring.datasource.url", mySQLContainer::getJdbcUrl);
+//         registry.add("spring.datasource.username", mySQLContainer::getUsername);
+//         registry.add("spring.datasource.password", mySQLContainer::getPassword);
+//         registry.add("spring.datasource.driver-class-name", mySQLContainer::getDriverClassName);
+//         // ensure Hibernate creates schema for tests
+//         registry.add("spring.jpa.hibernate.ddl-auto", () -> "create");
+//     }
+// }
