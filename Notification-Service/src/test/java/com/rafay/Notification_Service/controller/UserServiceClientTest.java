a6349abe809e145da8225d2ec4d2cb3dbb6b2c35
AbdulRafay-00 <iamrafay.00@gmail.com>
2026-08-17 23:22:51 +0500

    Notification-Service Feign Client Tested

---
+++ Notification-Service/src/test/java/com/rafay/Notification_Service/controller/UserServiceClientTest.java
@@ -0,0 +1,23 @@
+package com.rafay.Notification_Service.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvc;
+
+
+@SpringBootTest
+public class UserServiceClientTest {
+    @Autowired
+    MockMvc mockMvc;
+    @MockitoBean
+    private UserServiceClient userServiceClient;
+    
+    @Test
+    void testGetUserById() throws Exception {
+
+        mockMvc.perform(po)
+    }
+}
